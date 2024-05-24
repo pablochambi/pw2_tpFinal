2582d11be0da22d975b0b53ecc96357193467350
moavalos <mora_m_avalos@hotmail.com>
2024-05-23 23:26:38 -0300

    gitignore, vista perfil y algunas mejoras

--- model/RegistroModel.php
+++ model/RegistroModel.php
@@ -3,42 +3,40 @@
 class RegistroModel
 {
     private $database;
+
     public function __construct($database)
     {
         $this->database = $database;
     }
+
     public function registrarUsuarioAlaBD($nombre, $anio_nacimiento, $sexo,$pais,$ciudad,$email, $password,$username,$foto)
     {
         $token = bin2hex(random_bytes(8)); // Generar un token aleatorio
         $habilitado = 0;
 
-        $resultado = $this->database->executeAndReturn("INSERT INTO usuarios (nombre_completo, anio_nacimiento, sexo, pais, ciudad, email,password, username, foto,token,habilitado)
+        $resultado = $this->database->executeAndReturn(
+            "INSERT INTO usuarios (nombre_completo, anio_nacimiento, sexo, pais, ciudad, email,password, username, foto,token,habilitado)
 VALUES ('$nombre', '$anio_nacimiento', '$sexo', '$pais', '$ciudad', '$email', '$password', '$username', '$foto', '$token', '$habilitado')");
 
-        if(!$resultado){
+        if(!$resultado)
             echo "Error al registrar al usuario: " . mysqli_error($this->database->conn);
-        }
 
         return $token;
     }
 
     public function verificarYSubirLaFotoDePerfil($foto)
     {
-        if ($foto['error'] !== UPLOAD_ERR_OK) {
+        if ($foto['error'] !== UPLOAD_ERR_OK)
             echo "Error al cargar la imagen de perfil.";
-        }
 
         $direccionOrigen = $foto['tmp_name'];
         $direccionDestino = 'public/imagenes/' . basename($foto['name']);
 
-        if (!$this->subirFotoAUnaDireccion($direccionOrigen, $direccionDestino)) {
+        if (!$this->subirFotoAUnaDireccion($direccionOrigen, $direccionDestino))
             echo "Error al subir la imagen de perfil.";
-        }
 
         return $direccionDestino;
     }
-
-
 
     private function subirFotoAUnaDireccion($direccionOrigen,$direccionDestino) : bool
     {
@@ -58,20 +56,14 @@ VALUES ('$nombre', '$anio_nacimiento', '$sexo', '$pais', '$ciudad', '$email', '$
 
         if (count($result) == 1) {
             $updateSql = "UPDATE usuarios SET habilitado=1 WHERE token='$token'";
-            if ($this->database->executeAndReturn($updateSql)) {
+            if ($this->database->executeAndReturn($updateSql))
                 $mensaje =  "Cuenta validada correctamente.";
-            } else {
+            else
                 $mensaje = "Error al validar la cuenta.";
-            }
         } else {
             $mensaje =  "Token no válido o cuenta ya validada.";
         }
 
         return $mensaje;
     }
-
-
-
-
-
 }
