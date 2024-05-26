37388874df022d6a057733df531711b5724959c7
moavalos <mora_m_avalos@hotmail.com>
2024-05-26 20:08:04 -0300

    agrego perfilController para la vista de perfil

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -12,11 +12,17 @@ class LoginModel
     public function procesarInicioSesion($email, $password){
 
         $resultado = $this->database->prepare("SELECT * FROM usuario WHERE email = ?");
+        $resultado->bind_param("s", $email);
+        $resultado->execute();
+        $resultado = $resultado->get_result();
+
+        //$resultado = $this->database->prepare("SELECT * FROM usuario WHERE email = ?");
         if ($resultado -> num_rows > 0) {
             $fila = $resultado -> fetch_assoc();
             if (password_verify($password, $fila["password"])){
                 session_start();
                 $_SESSION["email"] = $email;
+                $_SESSION["user_id"] = $fila["id"];
                 header("location: futuro_home");
                 exit();
             }else{
@@ -29,16 +35,11 @@ class LoginModel
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public function obtenerDatosUsuario($userId)
+    {
+        $stmt = $this->database->prepare("SELECT * FROM usuario WHERE id = ?");
+        $stmt->bind_param("i", $userId);
+        $stmt->execute();
+        return $stmt->get_result()->fetch_assoc();
+    }
 }
