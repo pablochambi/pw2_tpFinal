f644bac3ffd74b0c19b654c5c9925f6e21168669
moavalos <mora_m_avalos@hotmail.com>
2024-06-04 16:03:59 -0300

    agrego funcion timer

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -12,26 +12,24 @@ class PartidaController extends BaseController
     {
         $this->checkSession();
 
-        if(isset($_POST['id_usuario'])) {
+        if (isset($_POST['id_usuario'])) {
             $id_usuario = $_POST['id_usuario'];
             $this->model->arrancarPartida($id_usuario);
         }
 
         $pregunta = $this->model->traerPreguntaAleatoria();
-        /*yo traia el ['id'] pero me tiraba error mire google y dice que tenes
-        que acceder al [0] para que te traiga el id de la primera consulta
-        */
         $respuestas = $this->model->traerRespuestasDesordenadas($pregunta[0]['id']);
 
         $this->presenter->render("view/partida.mustache", ['pregunta' => $pregunta, 'respuestas' => $respuestas]);
     }
 
-
     public function procesarRespuesta()
     {
         $this->checkSession();
 
-        if (isset($_POST['respuesta']) && isset($_POST['pregunta'])) {
+        if (isset($_POST['time_expired']) && $_POST['time_expired'] == "1") {
+            $this->handleTimeExpired(); // checkeo si se acabo el timepo
+        } elseif (isset($_POST['respuesta']) && isset($_POST['pregunta'])) {
             $respuesta = $_POST['respuesta'];
             $idPregunta = $_POST['pregunta'];
 
@@ -47,10 +45,14 @@ class PartidaController extends BaseController
             ];
 
             $this->presenter->render("view/esRespuestaCorrecta.mustache", $datos);
-
         } else {
             echo "No se encontró la respuesta o la pregunta en el formulario.";
         }
+    }
+
+    private function handleTimeExpired()
+    {
+        echo "El tiempo ha expirado. Has perdido la pregunta."; // ESTO DEBERIA SER UNA VISTA o controlarlo como mensaje pop up como mensajeValidacion.mustache
     }
 
     public function siguientePregunta()
@@ -58,9 +60,6 @@ class PartidaController extends BaseController
         $this->checkSession();
 
         $pregunta = $this->model->traerPreguntaAleatoria();
-        /*yo traia el ['id'] pero me tiraba error mire google y dice que tenes
-        que acceder al [0] ya que es una matriz asociativa para que te traiga el id de la primera consulta
-        */
 
         if (isset($pregunta[0]['id'])) {
             $respuestas = $this->model->traerRespuestasDesordenadas($pregunta[0]['id']);
@@ -75,30 +74,20 @@ class PartidaController extends BaseController
         $this->checkSession();
 
         if (isset($_POST['valor_respuesta']) && isset($_POST['id_pregunta'])) {
-
             $continuar = $_POST['valor_respuesta'];
             $id_pregunta = $_POST['id_pregunta'];
             if ($continuar == "Incorrecta") {
                 $user = $_SESSION['username'];
                 $puntaje = $this->model->obtenerCantidadDePuntos($user['id']);
                 $this->presenter->render("view/mostrarPuntajeDespuesPerder.mustache", ['puntaje' => $puntaje]);
-
             } else {
-                if(isset($_SESSION['username'])){
-                $user = $_SESSION['username'];
-                $partida = $this->model->obtenerUltimaPartida($user['id']);
-
-                /*$this->model->registrarPreguntaVistaPorElUsuario($id_pregunta,$user['id']);*/
-
-                $this->model->sumarPuntos($user['id'], $partida);
+                if (isset($_SESSION['username'])) {
+                    $user = $_SESSION['username'];
+                    $partida = $this->model->obtenerUltimaPartida($user['id']);
+                    $this->model->sumarPuntos($user['id'], $partida);
                 }
                 header("Location: /partida/siguientePregunta");
             }
-
         }
     }
-
-
 }
-
-
