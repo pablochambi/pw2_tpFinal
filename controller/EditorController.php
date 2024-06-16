f7492a8ed4b09f7b8f188a73d39663b99ff5e38b
pablochambi <pablochambi577@hotmail.com>
2024-06-16 17:23:57 -0300

    add botones, y tambien mostrar preguntas reportadas a un editor

--- controller/EditorController.php
+++ controller/EditorController.php
@@ -1,7 +1,6 @@
 <?php
 class EditorController extends BaseController
 {
-
     public function __construct($model, $presenter)
     {
         session_start();
@@ -10,39 +9,78 @@ class EditorController extends BaseController
 
     public function get()
     {
-        $this -> checkSession();
-        $user = $_SESSION['username'];
-
-        $rol = $this->verificarDeQueRolEsElUsuario($user['id']);
+        $user_id = $this->checkSessionYTraerIdUsuario();
+        $rol = $this->verificarDeQueRolEsElUsuario($user_id);
         $pregunta = $this ->model->traerPreguntasSugeridas();
-        $data = [
-            'pregunta' => $pregunta,
-            'rol' => $rol['rol'],
-        ];
-
-        $this->presenter->render('view/editor.mustache', $data);
-
+        $data = ['pregunta' => $pregunta, 'rol' => $rol['rol']];
+        $this->presenter->render('view/vistaEditor/editor.mustache', $data);
     }
-
     public function aceptar()
     {
         $this->checkSession();
-
         $idPregunta = $_GET['id'];
-
         $this->model->aceptarPreguntaSugerida($idPregunta);
-
         header('Location: /editor');
     }
-
     public function denegar()
 {
     $this->checkSession();
-
     $idPregunta = $_GET['id'];
-
-    $this->model->denegarPreguntaSugerida($idPregunta);
-
+    $this->model->eliminarPregunta($idPregunta);
     header('Location: /editor');
 }
+    public function mostrarPreguntasReportadas()
+    {
+        $idUsuario = $this->checkSessionYTraerIdUsuario();
+        $datos = $this->datosAEnviarALaVistaDeReportes($idUsuario);
+        $this->presenter->render("view/vistaEditor/preguntasReportadas.mustache",$datos);
+    }
+
+    public function mostrarTodasLasPreguntas()
+    {//Hacer
+        $user_id = $this->checkSessionYTraerIdUsuario();
+        $rol = $this->verificarDeQueRolEsElUsuario($user_id);
+
+        $preguntas = $this ->model->traerTodasLasPreguntas(); // Hacer metodo
+
+        $data = ['preguntas' => $preguntas, 'rol' => $rol['rol']];
+        $this->presenter->render("view/vistaEditor/todasLasPreguntas.mustache",$data);
+    }
+
+    public function aprobarPregunta()
+    {
+        $idUsuario = $this->checkSessionYTraerIdUsuario();
+        $idPregunta = $_GET['id'];
+        $this->model->eliminarPreguntaDeLaListaDeReportes($idPregunta,$idUsuario);
+
+        $datos = $this->datosAEnviarALaVistaDeReportes($idUsuario);
+        $this->presenter->render("view/vistaEditor/preguntasReportadas.mustache",$datos);
+    }
+    public function eliminarPregunta()
+    {
+        $idUsuario = $this->checkSessionYTraerIdUsuario();
+        $idPregunta = $_GET['id'];
+
+        $this->model->eliminarPreguntaDeLaListaDeReportes($idPregunta,$idUsuario);
+        $this->model->eliminarPregunta($idPregunta);
+
+        $datos = $this->datosAEnviarALaVistaDeReportes($idUsuario);
+
+        $this->presenter->render("view/vistaEditor/preguntasReportadas.mustache",$datos);
+    }
+    public function modificarPregunta()
+    {//Preguntar si se hace o no
+        $idUsuario = $this->checkSessionYTraerIdUsuario();
+        $idPregunta = $_GET['id'];
+    }
+
+
+    private function datosAEnviarALaVistaDeReportes($idUsuario): array
+    {
+        $rol = $this->verificarDeQueRolEsElUsuario($idUsuario);
+        $preguntas = $this->model->traerPreguntasReportadas();
+        return ['preguntas' => $preguntas, 'rol' => $rol['rol']];
+    }
+
+
 }
