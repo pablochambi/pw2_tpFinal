ddcef5598a677df4b55829ddd2007b707a576369
pablochambi <pablochambi577@hotmail.com>
2024-06-18 16:03:58 -0300

    se agrega la clase pdfCreator en el helper

--- helper/PdfCreator.php
+++ helper/PdfCreator.php
@@ -1,22 +1,14 @@
 <?php
-require_once 'dompdf/autoload.inc.php';
+require_once 'third-party/dompdf-example/dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
-
-class PdfCreator{
+class PdfCreator
+{
     public function create($html)
     {
-        // instantiate and use the dompdf class
         $dompdf = new Dompdf();
         $dompdf->loadHtml('hello world');
-
-        // (Optional) Setup the paper size and orientation
         $dompdf->setPaper('A4', 'landscape');
-
-        // Render the HTML as PDF
         $dompdf->render();
-
-        // Output the generated PDF to Browser
         $dompdf->stream("document.pdf" , ['Attachment' => 0]);
     }
 }
-
